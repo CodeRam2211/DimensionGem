7daf851d7482eadbf26afada7443286acc128337
ahmed-byte-sys <68530033+ahmed-byte-sys@users.noreply.github.com>
2023-12-10 05:49:21 +0530

    added death,traps teleportation
    Ps- flip is not working

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
 
     public int maxHealth = 100;
     public int currentHealth;
+    private Animator anim;
+    private Rigidbody2D rb;
 
     public HealthBar healthBar;
 
@@ -15,15 +18,13 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(20);
-        }
+        
     }
 
     void TakeDamage(int damage)
@@ -32,4 +33,33 @@ public class PlayerHealth : MonoBehaviour
 
         healthBar.SetHealth(currentHealth);
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("trap"))
+        {
+            TakeDamage(20);
+            Die();
+
+        }
+    }
+    
+
+    private void Die()
+    {
+        Debug.Log(currentHealth);
+        if (currentHealth == 0) 
+        {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+
+        }
+    }
+
+    private void RestartLvl()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 }
